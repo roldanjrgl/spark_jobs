2cb795455e62a5eef43520ad0d50b80019d87ff7
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2021-04-27 13:56:56 -0400

    program running

--- 2_word_count_pca_svd_exercises/PCA-Matrix/src/PCA.java
+++ 2_word_count_pca_svd_exercises/PCA-Matrix/src/PCA.java
@@ -1,15 +1,20 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Iterator;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.mllib.linalg.Matrix;
-import org.apache.spark.mllib.linalg.Vector;
-import org.apache.spark.mllib.linalg.Vectors;
-import org.apache.spark.mllib.linalg.distributed.RowMatrix;
+//import org.apache.spark.mllib.linalg.Matrix;
+//import org.apache.spark.mllib.linalg.Vector;
+//import org.apache.spark.mllib.linalg.Vectors;
+//import org.apache.spark.mllib.linalg.distributed.RowMatrix;
+
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.*;
+import scala.Tuple2;
 
 public class PCA {
     public static void main(String[] args) {
@@ -17,7 +22,36 @@ public class PCA {
         SparkContext sc = new SparkContext(conf);
         JavaSparkContext jsc = JavaSparkContext.fromSparkContext(sc);
 
+        JavaRDD<String> matrix = jsc.textFile(args[0], 1);
         
+        JavaRDD<String> words = matrix.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public Iterator<String> call(String s) {
+                return Arrays.stream(s.split(" ")).iterator();
+            }
+        });
+
+        JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(String s) {
+                return new Tuple2<>(s, 1);
+            }
+        });
+
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer i1, Integer i2) {
+                return i1 + i2;
+            }
+        });
+
+        List<Tuple2<String, Integer>> output = counts.collect();
+        for (Tuple2<String, Integer> tuple : output) {
+            System.out.println(tuple._1() + ": " + tuple._2());
+        }
+        jsc.stop();
+       
+        /*
         List<Vector> data = Arrays.asList(Vectors.sparse(5, new int[] { 1, 3 }, new double[] { 1.0, 7.0 }),
                 Vectors.dense(2.0, 0.0, 3.0, 4.0, 5.0), Vectors.dense(4.0, 0.0, 0.0, 6.0, 7.0));
 
@@ -40,5 +74,6 @@ public class PCA {
             System.out.println("\t" + vector);
         }
         jsc.stop();
+        */
     }
 }
